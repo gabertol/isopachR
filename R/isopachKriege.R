2c63d92a7cab180cff56ece47520027463f03fa2
Gabriel Bertolini <gabertol@gmail.com>
2023-07-29 12:25:15 -0300

    first commit add function IsopachKriege add vignette

---
+++ R/isopachKriege.R
@@ -0,0 +1,14 @@
+#' Isopachs with Kriege methods
+#'
+#' @param variables
+#'
+#' @return a raster map of isopachs
+#' @export
+#'
+#' @examples
+#' isopachKriege(XXXX)
+#'
+
+isopachKriege <- function(variables) {
+
+}
